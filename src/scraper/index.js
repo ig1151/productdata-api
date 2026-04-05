b4c4d582eb56a7f51ea94f834dd8df7cce47fb03
ig1151 <ig1151@hotmail.com>
2026-04-04 23:45:11 -0400

    Update index.js

--- src/scraper/index.js
+++ src/scraper/index.js
@@ -6,17 +6,16 @@ async function scrape(url) {
   const { hostname } = new URL(url);
 
   const shopify = await isShopify(hostname);
-  const data = shopify
-    ? await scrapeShopify(url)
-    : await scrapeGeneric(url);
 
-  if (!data || !data.title) {
-    const err = new Error('Could not extract product data');
-    err.code = 'PARSE_FAILURE';
-    throw err;
+  // Try Shopify first
+  if (shopify) {
+    try {
+      const data = await scrapeShopify(url);
+
+      if (data && data.title) {
+        return data;
+      }
+    } catch (err) {
+      console.warn('Shopify scrape failed, falling back to generic:', err.message);
+    }
   }
-
-  return data;
-}
-
-module.exports = { scrape };
